f43a791c2073acfccf8e80599246778350559c2b
pettTo <petttobias@gmail.com>
2021-02-17 15:25:56 +0100

    add samples; update decompress script

--- 000_scripts/decompressSamples.sh
+++ 000_scripts/decompressSamples.sh
@@ -6,7 +6,11 @@ PATH_CURRENT=$PWD
 # Switch to samples directory
 PATH_DATA=${PATH_CURRENT}/020_samples
 
+#iterate over years
+for year in ${PATH_DATA}/*; do
+	cd ${year}
 #iterate over all timestamps and decompress samples
-for timestamp in ${PATH_DATA}/*.tar.gz; do
-	tar -xzvf ${timestamp}
+	for timestamp in ${PATH_DATA}/*.tar.gz; do
+		tar -xzvf ${timestamp}
+	done
 done
